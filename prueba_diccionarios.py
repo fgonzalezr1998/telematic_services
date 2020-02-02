a1ce8de4c7fdcb4f00286f35c7e72159a855eaa1
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-02-02 21:15:44 +0100

    [prueba_diccionarios.py]changes added

--- prueba_diccionarios.py
+++ prueba_diccionarios.py
@@ -25,22 +25,83 @@ class MyDict():
                     if(i == v):
                         del self.map_table_[k]
 
+    def delete_by_key(self, key):
+        '''
+        key: key or keys list whose element we want to delete
+        '''
+        if(isinstance(key, list)):
+            for i in key:
+                self.delete_key_(i)
+        else:
+            self.delete_key_(key)
+
+    def add_value_2_keys(self, keys_list, value):
+
+        '''
+        keys_list: list of keys
+        value: value to add to dictionary
+
+        '''
+
+        for i in keys_list:
+
+            for k, v in self.map_table_.items():
+
+                if(k == i):
+                    if(isinstance(v, list)):
+                        v.append(value)
+
+                    else:
+                        l = [v, value]
+
+                        self.map_table_[k] = l
+
     def get_dic(self):
 
         return self.map_table_
 
+    def print_dic(self):
+        sorted_list = sorted(self.map_table_.items())
+        for i in sorted_list:
+            if(isinstance(i[1], list)):
+                print str(i[0]) + ": " + ", ".join(i[1])
+            else:
+                print str(i[0]) + ": " + str(i[1])
+
+    def delete_key_(self, key):
+        try:
+            del self.map_table_[key]
+        except KeyError:
+            pass
+
 if __name__ == "__main__":
 
-    dic = MyDict({'Francia': 'cerveza', 'Espana': ['vino', 'Paella', 'Tortilla de patata', 'Jamon']}) #Defino el objeto dic de la clase MyDict
+    #creo la lista
+    dic = MyDict({'valencia': ['unacosa', 'otracosa'], 'Francia': 'cerveza', 'Espana': ['vino', 'Tortilla de patata', 'Jamon']}) #Defino el objeto dic de la clase MyDict
 
     print(dic.get_dic())
 
+    #anado un elemento
     dic.add_element('Inglaterra', 'Fish & Chips')
 
     print(dic.get_dic())
 
+    #borro todos los elementos incluidos en la lista 'alcoholicas'
     alcoholicas = ['cerveza', 'vino', 'sidra']
 
     dic.delete_value_by_list(alcoholicas)
 
     print(dic.get_dic())
+
+    #Anado el valor 'paella' a cualquiera de las claves incluidas en la lista 'comunidad_valenciana'
+    comunidad_valenciana = ['valencia', 'alicante', 'castellon']
+
+    dic.add_value_2_keys(comunidad_valenciana, 'paella')
+    print(dic.get_dic())
+
+    #borro los elementos cuyas claves se encuentran en la lista 'comunidad_valenciana'
+    dic.delete_by_key(comunidad_valenciana)
+    print(dic.get_dic())
+    print("-------------")
+
+    dic.print_dic()
