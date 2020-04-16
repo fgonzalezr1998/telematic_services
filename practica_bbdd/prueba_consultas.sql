ccb1bd05a0468246754f61e85410620db5a90784
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-04-16 22:04:43 +0200

    [xml]changes added

--- practica_bbdd/prueba_consultas.sql
+++ practica_bbdd/prueba_consultas.sql
@@ -3,7 +3,7 @@
 SELECT Nombre, Apellido1, Apellido2, Marca, Modelo, Matricula
   FROM Clientes, Automoviles
   WHERE Clientes.DNI = Automoviles.DNI_Dueno;
-  
+
 -- 2. Horas trabajadas por empleado y mes
 SELECT *
   FROM Automoviles;
