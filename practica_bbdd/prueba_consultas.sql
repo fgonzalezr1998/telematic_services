d071588bae61307db47e6b332bb5f19133f5b6e3
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-04-19 00:09:10 +0200

    [practica_bbdd]pruebas_consultas.sql modified

--- practica_bbdd/prueba_consultas.sql
+++ practica_bbdd/prueba_consultas.sql
@@ -1,9 +1,44 @@
-
+/*
 -- 1. Listado de automoviles por clientes
 SELECT Nombre, Apellido1, Apellido2, Marca, Modelo, Matricula
   FROM Clientes, Automoviles
   WHERE Clientes.DNI = Automoviles.DNI_Dueno;
 
 -- 2. Horas trabajadas por empleado y mes
+SELECT Nombre, Apellido1, Apellido2, Empleados.DNI, Suma_Horas, Mes
+  FROM(
+    SELECT DNI_Empleado AS'DNI', sum(Horas) AS'Suma_Horas', Mes
+      FROM Facturas
+        GROUP BY DNI, Mes
+  ) AS'Subconsulta', Empleados
+
+  WHERE Empleados.DNI = Subconsulta.DNI
+
+--3. Media de horas que invierte cada empleado en cada trabajo distinto
+SELECT Nombre, Apellido1, Apellido2, Empleados.DNI, Media_Horas, ID_Trabajo
+  FROM(
+      SELECT DNI_Empleado AS'DNI', avg(Horas) AS'Media_Horas', ID_Trabajo
+        FROM Facturas
+        GROUP BY DNI, ID_Trabajo
+  ) AS'Subconsulta', Empleados
+
+  WHERE Empleados.DNI = Subconsulta.DNI
+
+
+-- 4. Consultar Dinero gastado por clientes
+
+SELECT (SELECT Precio_Ud * NV FROM Repuestos WHERE Nombre = "Ventanas"),
+        (SELECT Precio_Ud * NP FROM Repuestos WHERE Nombre = "Puertas"),
+        (SELECT Precio_Ud * NR FROM Repuestos WHERE Nombre = "Ruedas"),
+        (SELECT Precio_Ud * NM FROM Repuestos WHERE Nombre = "Motores"),
+        DNI_Cliente
+  FROM(
+      SELECT DNI_Cliente, sum(Num_Ventanas) AS'NV', sum(Num_Puertas) AS'NP', sum(Num_Ruedas) AS'NR', sum(Num_Motores) AS'NM'
+        FROM Facturas
+        GROUP BY DNI_Cliente
+  )
+*/
+-- 5.Consultar las facturas pendientes
 SELECT *
-  FROM Automoviles;
+  FROM Facturas
+  WHERE Cobrada = 0
