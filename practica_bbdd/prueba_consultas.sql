f3ccead5350fe35b53726453b56e61ec3d65d07a
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-04-19 14:35:09 +0200

    [practica_bbdd]prueba_consultas.sql changes added

--- practica_bbdd/prueba_consultas.sql
+++ practica_bbdd/prueba_consultas.sql
@@ -1,10 +1,10 @@
 /*
--- 1. Listado de automoviles por clientes
+-- 1.Listado de automoviles por clientes
 SELECT Nombre, Apellido1, Apellido2, Marca, Modelo, Matricula
   FROM Clientes, Automoviles
   WHERE Clientes.DNI = Automoviles.DNI_Dueno;
 
--- 2. Horas trabajadas por empleado y mes
+-- 2.Horas trabajadas por empleado y mes
 SELECT Nombre, Apellido1, Apellido2, Empleados.DNI, Suma_Horas, Mes
   FROM(
     SELECT DNI_Empleado AS'DNI', sum(Horas) AS'Suma_Horas', Mes
@@ -14,7 +14,7 @@ SELECT Nombre, Apellido1, Apellido2, Empleados.DNI, Suma_Horas, Mes
 
   WHERE Empleados.DNI = Subconsulta.DNI
 
---3. Media de horas que invierte cada empleado en cada trabajo distinto
+-- 3.Media de horas que invierte cada empleado en cada trabajo distinto
 SELECT Nombre, Apellido1, Apellido2, Empleados.DNI, Media_Horas, ID_Trabajo
   FROM(
       SELECT DNI_Empleado AS'DNI', avg(Horas) AS'Media_Horas', ID_Trabajo
@@ -25,7 +25,7 @@ SELECT Nombre, Apellido1, Apellido2, Empleados.DNI, Media_Horas, ID_Trabajo
   WHERE Empleados.DNI = Subconsulta.DNI
 
 
--- 4. Consultar Dinero gastado por clientes
+-- 4.Consultar Dinero gastado por clientes
 
 SELECT (SELECT Precio_Ud * NV FROM Repuestos WHERE Nombre = "Ventanas"),
         (SELECT Precio_Ud * NP FROM Repuestos WHERE Nombre = "Puertas"),
@@ -37,8 +37,17 @@ SELECT (SELECT Precio_Ud * NV FROM Repuestos WHERE Nombre = "Ventanas"),
         FROM Facturas
         GROUP BY DNI_Cliente
   )
-*/
+
 -- 5.Consultar las facturas pendientes
 SELECT *
   FROM Facturas
   WHERE Cobrada = 0
+*/
+
+-- 6.Consultar facturas pendientes por cliente
+
+SELECT DNI_Cliente, Nombre, Apellido1, Apellido2, Cobrada
+  FROM Facturas, Clientes
+  --GROUP BY DNI_Cliente
+  --HAVING DNI_Cliente = Clientes.DNI and Cobrada = 0
+  WHERE DNI_Cliente = Clientes.DNI and Cobrada = 0
